972b740f1454dc710ccc16fcd4ae6a054d40d066
Abhishek Upadhyay <Abhishekupadhyay153@gmail.com>
2018-07-30 10:35:26 +0530

    first

---
+++ app/src/main/java/com/example/abhishek/aidl/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.abhishek.aidl
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*val ps = ProductService()
+
+        val products: List<Product> = ps.getlist()
+
+
+        for (producst in products) {
+
+        }*/
+    }
+}
